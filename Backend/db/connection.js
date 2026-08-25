47bace45d24f48311ca41bbc858633c5ef2fcc56
SavanSavaliya07 <saavansavaliya702@gmail.com>
2026-08-25 11:18:39 +0530

    Clean install - fix rolldown issues for Render deployment

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
-
+const MONGO_URI =
+  process.env.MONGO_URI ||
+  // "mongodb+srv://saavansavaliya702_db_user:7KI5YJ8K743UFR45@workers.iseuvax.mongodb.net/Company-Calculation=workers";
+  "mongodb+srv://saavansavaliya702_db_user:7KI5YJ8K743UFR45@workers.iseuvax.mongodb.net/Company-Calculation";
 mongoose
-  .connect("mongodb://localhost:27017/HMproject")
+  .connect(MONGO_URI)
   .then(() => {
     console.log(colors.america("MongoDB is successfully connected"));
   })
