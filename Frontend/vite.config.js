bff078131b362d0975a69f9ee0d17132bb9acdc7
SavanSavaliya702 <saavansavaliya702@gmail.com>
2026-08-25 10:25:12 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "terser",
+  },
 })
