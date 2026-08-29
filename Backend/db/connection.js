6e38cc7da54878f8d4b6bb474e21e89331cd9fbd
SavanSavaliya702 <saavansavaliya702@gmail.com>
2026-08-29 09:33:56 +0530

    Update connection.js
    Removed hardcoded MongoDB connection string.

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const MONGO_URI =
   process.env.MONGO_URI ||
-  "mongodb+srv://saavansavaliya702_db_user:7KI5YJ8K743UFR45@workers.iseuvax.mongodb.net/?appName=workers";
-  // "mongodb+srv://saavansavaliya702_db_user:7KI5YJ8K743UFR45@workers.iseuvax.mongodb.net/Company-Calculation";
+  
 
   
 mongoose
